204ac8b93d2a3e8845ed5a768c69bd365d1a1f89
yc18841 <y_chen_98@yahoo.com>
2025-03-27 16:00:42 -0400

    initial commit

---
+++ child-service/src/main/java/com/chen/childservice/ChildServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chen.childservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildServiceApplication.class, args);
+	}
+
+}
---
+++ child-service/src/test/java/com/chen/childservice/ChildServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.childservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
