5037b626d11e665bdfebca1db5fccd14ee6ac308
yc18841 <y_chen_98@yahoo.com>
2025-03-26 22:52:39 -0400

    initial commit

---
+++ parent-service/src/test/java/com/chen/parentservice/ParentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.parentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
