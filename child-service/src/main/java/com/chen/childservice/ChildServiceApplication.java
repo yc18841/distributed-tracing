7f8fca9a7c1f4db660934cbc86a06d67f5659ee4
yc18841 <y_chen_98@yahoo.com>
2025-03-27 18:03:25 -0400

    create controller

--- child-service/src/main/java/com/chen/childservice/ChildServiceApplication.java
+++ child-service/src/main/java/com/chen/childservice/ChildServiceApplication.java
@@ -2,6 +2,9 @@ package com.chen.childservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ChildServiceApplication {
@@ -10,4 +13,8 @@ public class ChildServiceApplication {
 		SpringApplication.run(ChildServiceApplication.class, args);
 	}
 
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
---
+++ child-service/src/main/java/com/chen/childservice/ObserverConfig.java
@@ -0,0 +1,15 @@
+package com.chen.childservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+
+@Configuration(proxyBeanMethods = false)
+public class ObserverConfig {
+	@Bean
+    ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
+    }
+}
