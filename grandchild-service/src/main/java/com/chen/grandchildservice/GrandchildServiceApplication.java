a0082d570d6661fd7b7209bd3347bb3e90aa6ab4
yc18841 <y_chen_98@yahoo.com>
2025-03-27 23:01:44 -0400

    initial commit

---
+++ grandchild-service/src/main/java/com/chen/grandchildservice/GrandchildServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chen.grandchildservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandchildServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandchildServiceApplication.class, args);
+	}
+
+}
